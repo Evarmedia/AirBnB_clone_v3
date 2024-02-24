b75c31d4bfb24cc485c33f93b53a06ee97b59e5f
Evarmedia <mishakmanuel@gmail.com>
2024-02-24 15:33:19 +0100

    reset

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,50 +1,36 @@
 #!/usr/bin/python3
-'''Contains a Flask web application API.
-'''
+"""Flask Api application."""
+
 import os
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 
 from models import storage
 from api.v1.views import app_views
 
-
+"""Creatingg a variable app instance of Flask."""
 app = Flask(__name__)
-'''The Flask web application instance.'''
+"""Creatingg a variable app instance of Flask."""
 app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+app_host = int(os.getenv('HBNB_API_PORT', '5000'))
+"""Enforce strict trally slashes on routes."""
 app.url_map.strict_slashes = False
+"""Register the blueprint app_views to Flask instance app."""
 app.register_blueprint(app_views)
 CORS(app, resources={'/*': {'origins': app_host}})
 
-
 @app.teardown_appcontext
-def teardown_flask(exception):
-    '''The Flask app/request context end event listener.'''
-    # print(exception)
+def teardown_engine(exception):
+    """Remove current SQLAlchemy session object after each request."""
     storage.close()
 
 
-@app.errorhandler(404)
-def error_404(error):
-    '''Handles the 404 HTTP error code.'''
-    return jsonify(error='Not found'), 404
+if __name__ == "__main__":
 
-
-@app.errorhandler(400)
-def error_400(error):
-    '''Handles the 400 HTTP error code.'''
-    msg = 'Bad request'
-    if isinstance(error, Exception) and hasattr(error, 'description'):
-        msg = error.description
-    return jsonify(error=msg), 400
-
-
-if __name__ == '__main__':
     app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app_host = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(
         host=app_host,
-        port=app_port,
+        port=app_host,
         threaded=True
-    )
+        )
