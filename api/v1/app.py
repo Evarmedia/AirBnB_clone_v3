e6dac8e6e52c8d1c024f6b786dc66863a8bebd08
Evarmedia <mishakmanuel@gmail.com>
2024-02-24 15:10:11 +0100

    debugging

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,22 +2,23 @@
 """Flask Api application."""
 
 from os import getenv
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
+
 from models import storage
 from api.v1.views import app_views
 
 """Creatingg a variable app instance of Flask."""
+
 app = Flask(__name__)
-
-"""Enabling cors and allowing origins."""
-CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
-
-"""Register the blueprint app_views to Flask instance app."""
-app.register_blueprint(app_views)
-
+"""Creatingg a variable app instance of Flask."""
+HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+PORT = int(getenv('HBNB_API_PORT', '5000'))
 """Enforce strict trally slashes on routes."""
 app.url_map.strict_slashes = False
+"""Register the blueprint app_views to Flask instance app."""
+app.register_blueprint(app_views)
+CORS(app, resources={'/*': {'origins': HOST}})
 
 
 def teardown_engine(exception):
@@ -29,4 +30,8 @@ if __name__ == "__main__":
 
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+    app.run(
+        host=HOST,
+        port=PORT,
+        threaded=True
+        )
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Views for Airbnb Api."""
-from api.v1.views import app_views
 from flask import jsonify
+from api.v1.views import app_views
 
 
-@app_views.route('/status', method=['GET'])
+@app_views.route('/status')
 def status():
     """Returns a JSON response for RESTful Api."""
     return jsonify(status="OK")
