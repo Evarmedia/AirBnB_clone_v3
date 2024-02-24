1eb45445ba0515d0c6a80c7fda87487a85a69930
TatendaTorerwa <ttorerwa01@gmail.com>
2024-02-24 05:29:24 -0800

    3. Status of your API.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', method=[GET])
+@app_views.route('/status', method=['GET'])
 def status():
     """Returns a JSON response for RESTful Api."""
     return jsonify(status="OK")
