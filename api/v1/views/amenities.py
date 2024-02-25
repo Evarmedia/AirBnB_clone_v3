ba5655455bea62f4e97d703fd8424c49d21daefd
Evarmedia <mishakmanuel@gmail.com>
2024-02-25 22:22:02 +0100

    Amenity#8

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,74 +1,80 @@
 #!/usr/bin/python3
+''' Amenity objects that handles all default RESTFul API actions.'''
+
 from flask import abort, jsonify, request
 from models.amenity import Amenity
 from models import storage
 from api.v1.views import app_views
 
 
-@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
-def get_all_states():
-    """Get all states objects from the storage."""
-    states = storage.all(State).values()
+@app_views.route('/amenities', methods=['GET'],
+                 strict_slashes=False)
+def get_all_amenities():
+    """Get all amenities objects from the storage."""
+    amenities = storage.all(Amenity).values()
     """Convert the object to dict and JSON."""
-    state_list = [state.to_dict() for state in states]
+    amenities_list = [amenity.to_dict() for amenity in amenities]
 
-    return jsonify(state_list)
+    return jsonify(amenities_list)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def get_state(state_id):
-    """"Get the state object with the given id from storage."""
-    state = storage.get(State, state_id)
-    """Return the state object in JSON otherwise 404 error."""
-    if state:
-        return jsonify(state.to_dict())
+@app_views.route('/amenities/<amenities_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_single_amenities(amenities_id):
+    """"Get the amenities object with the given id from storage."""
+    amenities = storage.get(Amenity, amenities_id)
+    """Return the amenities object in JSON otherwise 404 error."""
+    if amenities:
+        return jsonify(amenities.to_dict())
     else:
         abort(404)
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'])
-def delete_state(state_id):
-    state = storage.get(State, state_id)
-    if state:
-        """Delete the state object from storege and save changes
+@app_views.route('/amenities/<amenities_id>', methods=['DELETE'])
+def delete_amenities(amenities_id):
+    amenities = storage.get(Amenity, amenities_id)
+    if amenities:
+        """Delete the amenities object from storege and save changes
            and return empty JSON with status code 200
            otherwise return 404 error.
         """
-        storage.delete(state)
+        storage.delete(amenities)
         storage.save()
         return jsonify({}), 200
     else:
         abort(404)
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
-def add_state():
+@app_views.route('/amenities', methods=['POST'],
+                 strict_slashes=False)
+def add_amenities():
     """If the request data is not in JSON format
        and if 'name' key is missing in the JSON
-       data return error 400.Otherwise add new State
+       data return error 400.Otherwise add new Amenity
        object with the JSON data
     """
     data = request.get_json()
     if not data or 'name' not in data:
         abort(400, 'Not a JSON or Missing name')
 
-    state = State(**data)
-    state.save()
+    amenities = Amenity(**data)
+    amenities.save()
     """ Return the newly created State object
         in JSON format with 201 status code.
     """
-    return jsonify(state.to_dict()), 201
+    return jsonify(amenities.to_dict()), 201
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def update_state(state_id):
+@app_views.route('/amenities/<amenities_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_amenities(amenities_id):
     """Updates a State object.
        Get the State object with
        the given ID from the storage.
      """
 
-    state = storage.get(State, state_id)
-    if not state:
+    amenities = storage.get(Amenity, amenities_id)
+    if not amenities:
         """Return 404 if state is not found."""
         abort(404)
 
@@ -77,17 +83,17 @@ def update_state(state_id):
         """Return 400 if request data is not JSON."""
         abort(400, 'Not a JSON')
 
-    """Update the attributes of the State object with the JSON data."""
+    """Update the attributes of the Amenity object with the JSON data."""
     ignore_keys = ['id', 'created_at', 'updated_at']
     for key, value in data.items():
         if key not in ignore_keys:
-            setattr(state, key, value)
+            setattr(amenities, key, value)
     """Save the updated State object to the storage
        Return the updated State object in JSON format
        with 200 status code.
     """
-    state.save()
-    return jsonify(state.to_dict()), 200
+    amenities.save()
+    return jsonify(amenities.to_dict()), 200
 
 
 """Error Handlers."""
