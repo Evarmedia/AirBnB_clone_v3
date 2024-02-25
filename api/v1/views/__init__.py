a9516701cff623718223c805e75d1ddc575e0238
Evarmedia <mishakmanuel@gmail.com>
2024-02-25 09:24:14 +0100

    #2

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """Creating a blueprint for the Api."""
-
 from flask import Blueprint
+
+# Creating a Blueprint instance with url prefix /api/v1
+app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
 
 # import all views from index module
 from api.v1.views.index import *
-
-
-# Creating a Blueprint instance with url prefix /api/v1
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,8 +6,11 @@ from models import storage
 
 
 # Create route /status on the object app_views
-@app_views.route('/status')
+@app_views.route('/status' strict_slashes=False)
 def status():
     """Returns a JSON response for RESTful Api."""
     response = {'status': 'OK'}
     return jsonify(response)
+
+if __name__ == "__main__":
+    pass
