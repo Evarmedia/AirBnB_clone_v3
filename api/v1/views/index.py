2e43f52aa3331811b2f371189a31ba4e8e898c81
TatendaTorerwa <ttorerwa01@gmail.com>
2024-02-25 00:18:38 -0800

    4. Some stats? 5. Not found.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,19 @@ def status():
     """Returns a JSON response for RESTful Api."""
     response = {'status': 'OK'}
     return jsonify(response)
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """
+    Retrieves the number of each objects by type.
+    """
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
+
