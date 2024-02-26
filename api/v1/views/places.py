527bf06eac5bb25e2e0abbbab4aff7104cc498a1
Evarmedia <mishakmanuel@gmail.com>
2024-02-26 14:01:28 +0100

    search

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,11 +5,12 @@ from flask import abort, jsonify, request
 from models.place import Place
 from models.city import City
 from models.user import User
+from models.state import State
 from models import storage
 from api.v1.views import app_views
 
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'],
+@app_views.route('/d_cities/<city_id>/places', methods=['GET'],
                  strict_slashes=False)
 def get_all_places(city_id):
     ''' Get all places in the specified city'''
@@ -45,7 +46,7 @@ def delete_a_place(place_id):
     return jsonify({}), 200
 
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'],
+@app_views.route('/d_cities/<city_id>/places', methods=['POST'],
                  strict_slashes=False)
 def create_place(city_id):
     ''' Create a new place'''
@@ -96,6 +97,49 @@ def update_place(place_id):
 
 
 """Error Handlers."""
+
+
+@app_views.route('/places_search', methods=['POST'])
+def search_places():
+    # Check if request body is valid JSON
+    if not request.is_json:
+        abort(400, description="Not a JSON")
+
+    # Parse JSON body
+    data = request.get_json()
+
+    # Extract optional keys
+    d_states = data.get('d_states', [])
+    d_cities = data.get('d_cities', [])
+    d_amenities = data.get('d_amenities', [])
+
+    # Retrieve all places if the lists are empty
+    if not d_states and not d_cities:
+        places = storage.all(Place).values()
+    else:
+        # Retrieve places based on d_states and d_cities
+        place_ids = set()
+        for state_id in d_states:
+            state = storage.get(State, state_id)
+            if state:
+                place_ids.update([city.id for city in state.d_cities])
+
+        for city_id in d_cities:
+            city = storage.get(City, city_id)
+            if city:
+                place_ids.add(city.id)
+
+        places = [storage.get(Place, place_id) for place_id in place_ids if storage.get(Place, place_id)]
+
+    # Filter places based on d_amenities
+    if d_amenities:
+        filtered_places = []
+        for place in places:
+            if all(amenity_id in place.d_amenities for amenity_id in d_amenities):
+                filtered_places.append(place)
+        places = filtered_places
+
+    return jsonify([place.to_dict() for place in places])
 
 
 @app_views.errorhandler(404)
