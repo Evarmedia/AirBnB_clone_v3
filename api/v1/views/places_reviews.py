2b6d19ad18caf7a068556971ab50920937059df1
Evarmedia <mishakmanuel@gmail.com>
2024-02-26 10:22:24 +0100

    places_review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,1 +1,104 @@
 #!/usr/bin/python3
+from flask import Flask, jsonify, request, abort
+from api.v1.views import app_views
+from models import storage
+from models.review import Review
+from models.place import Place
+from models.user import User
+
+
+@app_views.route('/places/<place_id>/reviews', methods=['GET'])
+def get_reviews_by_place(place_id):
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+
+    reviews = [review.to_dict() for review in place.reviews]
+    return jsonify(reviews)
+
+
+@app_views.route('/reviews/<review_id>', methods=['GET'])
+def get_review(review_id):
+    review = storage.get(Review, review_id)
+    if review is None:
+        abort(404)
+
+    return jsonify(review.to_dict())
+
+
+@app_views.route('/reviews/<review_id>', methods=['DELETE'])
+def delete_review(review_id):
+    review = storage.get(Review, review_id)
+    if review is None:
+        abort(404)
+
+    storage.delete(review)
+    storage.save()
+
+    return jsonify({}), 200
+
+
+@app_views.route('/places/<place_id>/reviews', methods=['POST'])
+def create_review(place_id):
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+
+    if not request.json:
+        abort(400, 'Not a JSON')
+
+    data = request.get_json()
+    if 'user_id' not in data:
+        abort(400, 'Missing user_id')
+    user_id = data['user_id']
+    if storage.get(User, user_id) is None:
+        abort(404)
+
+    if 'text' not in data:
+        abort(400, 'Missing text')
+
+    data['place_id'] = place_id
+    review = Review(**data)
+    review.save()
+
+    return jsonify(review.to_dict()), 201
+
+
+@app_views.route('/reviews/<review_id>', methods=['PUT'])
+def update_review(review_id):
+    review = storage.get(Review, review_id)
+    if review is None:
+        abort(404)
+
+    if not request.json:
+        abort(400, 'Not a JSON')
+
+    data = request.get_json()
+    ignore_keys = ['id', 'user_id', 'place_id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(review, key, value)
+    review.save()
+
+    return jsonify(review.to_dict()), 200
+
+
+"""Error Handlers."""
+
+
+@app_views.errorhandler(404)
+def not_found(error):
+    """
+    Returns a JSON response for 404 error (Not Found).
+    """
+    response = {'error': 'Not found'}
+    return jsonify(response), 404
+
+
+@app_views.errorhandler(400)
+def bad_request(error):
+    """
+    Returns a JSON response for 400 error (Bad Request).
+    """
+    response = {'error': 'Bad Request'}
+    return jsonify(response), 400
