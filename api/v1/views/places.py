9dc08ff93a22aa9ea994e638ec13e41ee31bcaf8
Evarmedia <mishakmanuel@gmail.com>
2024-02-26 09:29:41 +0100

    10-Places refactoring

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -11,18 +11,21 @@ from api.v1.views import app_views
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
                  strict_slashes=False)
-def get_all_cities(city_id):
+def get_all_places(city_id):
+    ''' Get all places in the specified city'''
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
-
-    places_list = [place.to_dict() for place in city.places]
-    return jsonify(places_list)
+    places = []
+    for place in city.places:
+        places.append(place.to_dict())
+    return jsonify(places)
 
 
 @app_views.route('/places/<place_id>', methods=['GET'],
                  strict_slashes=False)
 def get_single_place(place_id):
+    ''' Returns a single place'''
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
@@ -32,6 +35,7 @@ def get_single_place(place_id):
 
 @app_views.route('/places/<place_id>', methods=['DELETE'])
 def delete_a_place(place_id):
+    ''' Delete a place'''
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
@@ -44,19 +48,20 @@ def delete_a_place(place_id):
 @app_views.route('/cities/<city_id>/places', methods=['POST'],
                  strict_slashes=False)
 def create_place(city_id):
+    ''' Create a new place'''
     city = storage.get(City, city_id)
 
     if city is None:
         abort(404)
 
-    data = request.get_json()
-
     if not request.json:
         abort(400, 'Not a JSON')
 
+    data = request.get_json()
+
     if 'user_id' not in data:
         abort(400, 'Missing user_id')
-    
+
     user = storage.get(User, data['user_id'])
 
     if user is None:
