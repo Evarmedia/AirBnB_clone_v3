2cb9cc824068f4ec6f21dace854d41e507db8efa
TatendaTorerwa <ttorerwa01@gmail.com>
2024-02-26 03:53:06 -0800

    CORS.

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,7 +12,7 @@ from api.v1.views import app_views
 app = Flask(__name__)
 
 # enable CORS and allow for origins:
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0:"}})
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0:"}})
 
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
